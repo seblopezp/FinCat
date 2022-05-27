cb79222132be8c5e5fbdb6c4f05ad992eb158c08
seblopezp <seblopezp@gmail.com>
2022-05-26 22:36:42 -0400

    ADD: stack navigator

---
+++ src/navigator/StackNavigator.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../features/home/containers/Home';
+import Login from '../features/login/containers/Login';
+
+const Stack = createNativeStackNavigator();
+
+export const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Login"
+        component={Login}
+      />
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
+};
+export default StackNavigator;
