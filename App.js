b15b7b5ee545c0dda1d3279ecab068b03106d636
seblopezp <seblopezp@gmail.com>
2022-06-07 21:22:48 -0400

    ADD: base theme

--- App.js
+++ App.js
@@ -2,11 +2,40 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigator from './src/navigator/StackNavigator';
 import { NativeBaseProvider } from 'native-base';
+import { extendTheme } from 'native-base';
+
 export default function App() {
+
+  const theme = extendTheme({
+    colors: {
+      // Add new color
+      primary: {
+        50: '#E3F2F9',
+        100: '#C5E4F3',
+        200: '#A2D4EC',
+        300: '#7AC1E4',
+        400: '#47A9DA',
+        500: '#0088CC',
+        600: '#007AB8',
+        700: '#006BA1',
+        800: '#005885',
+        900: '#003F5E'
+      },
+      // Redefinig only one shade, rest of the color will remain same.
+      amber: {
+        400: '#d97706'
+      }
+    },
+    config: {
+      // Changing initialColorMode to 'dark'
+      initialColorMode: 'dark'
+    }
+  });
+
   return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-        <StackNavigator />
+    <NativeBaseProvider theme={theme}>
+      <NavigationContainer  >
+        <StackNavigator  />
       </NavigationContainer>
     </NativeBaseProvider>
   );
