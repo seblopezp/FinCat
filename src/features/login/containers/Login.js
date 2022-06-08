615ce2ca389de1a0f7a357e61ec8a9bc9dc354bf
Rekzide <b.villa@outlook.es>
2022-06-08 00:40:40 -0400

    feat: se avanza en diseño

--- src/features/login/containers/Login.js
+++ src/features/login/containers/Login.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
-import {Center, Container, Heading} from 'native-base';
+import { StyleSheet } from 'react-native';
+import { Box, Center, Container, Heading, Stack, Text, VStack, Input, Flex } from 'native-base';
+import Styles from '../containers/Styles';
 
 const Login = () => {
   return (
-    <Center bg="primary.500" p="4" style={styles.flex1}>
-      <Container>
-        <Heading>
-          <Text>Hola</Text>
-          <Text color="emerald.500"> React Ecosystem</Text>
-        </Heading>
-        <Text mt="3" fontWeight="medium">
-          NativeBase is a simple, modular and accessible component library that
-          gives you building blocks to build you React applications.
-        </Text>
+    <Center bg="purple.500" flex={1} p="1">
+      <Container bg="purple.400" borderRadius="md" w="100%" >
+        <Heading />
+        <Flex  h="50%" alignItems="center" >
+          <Box >
+            <Center size={90} bg="primary.500">
+              <Text bold fontSize="3xl" fontStyle="italic">FinCat</Text>
+            </Center>
+            <Stack >
+              <Input size="lg" variant="rounded" placeholder="lg Input" />
+              <Input size="lg" variant="rounded" placeholder="lg Input" />
+            </Stack>
+          </Box>
+        </Flex>
       </Container>
     </Center>
   );
 };
-
-const styles = StyleSheet.create({
-  flex1: {
-    flex: 1,
-  },
-});
 
 export default Login;
---
+++ src/features/login/containers/Styles.js
@@ -0,0 +1,17 @@
+
+import {StyleSheet} from 'react-native';
+
+
+const styles = StyleSheet.create({
+    box: {
+        alignItems: 'center',
+    },
+    flex1: {
+      flex: 1,
+    },
+    title: {
+        fontFamily: ""
+    }
+  });
+
+export default styles;
