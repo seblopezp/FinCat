5e9e21b3765fd6a6947565b9fb0fb87aa656cdb9
seblopezp <seblopezp@gmail.com>
2022-06-07 21:25:53 -0400

    ADD: se agrega base para login

--- src/features/login/containers/Login.js
+++ src/features/login/containers/Login.js
@@ -1,7 +1,28 @@
 import React from 'react';
-import { Text, StyleSheet } from "react-native";
+import {Text, StyleSheet} from 'react-native';
+import {Center, Container, Heading} from 'native-base';
+
 const Login = () => {
-  return <Text>Login</Text>;
+  return (
+    <Center bg="primary.500" p="4" style={styles.flex1}>
+      <Container>
+        <Heading>
+          <Text>Hola</Text>
+          <Text color="emerald.500"> React Ecosystem</Text>
+        </Heading>
+        <Text mt="3" fontWeight="medium">
+          NativeBase is a simple, modular and accessible component library that
+          gives you building blocks to build you React applications.
+        </Text>
+      </Container>
+    </Center>
+  );
 };
+
+const styles = StyleSheet.create({
+  flex1: {
+    flex: 1,
+  },
+});
 
 export default Login;
