3fafb9bee45f20eb1e5a0a2fa67b3257e357a968
seblopezp <seblopezp@gmail.com>
2022-06-28 22:04:24 -0400

    UPDATE: se quita logica de login y se lleva a componente

--- src/features/login/containers/Login.js
+++ src/features/login/containers/Login.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Center, Text, VStack, Input, Flex, Stack} from 'native-base';
+import {Center, Text, VStack, Flex} from 'native-base';
 import {Image, StyleSheet} from 'react-native';
-import {Button} from 'native-base';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { LoginForm } from '../components/LoginForm';
+
 const Login = () => {
   const styles = StyleSheet.create({
     tinyLogo: {
@@ -11,47 +11,7 @@ const Login = () => {
       width: 220,
     },
   });
-
-  const InputForm = () => {
-    const [show, setShow] = React.useState(false);
-    return (
-      <Stack space={4} w="100%" alignItems="center">
-        <Input
-          color="black.100"
-          mb="1"
-          px="2"
-          variant={'underlined'}
-          w={{
-            base: '100%',
-            md: '25%',
-          }}
-          InputLeftElement={<Icon name="at" size={20} ml="2" />}
-          placeholder="Email ID"
-        />
-        <Input
-          color="black.100"
-          px="2"
-          variant={'underlined'}
-          w={{
-            base: '100%',
-            md: '25%',
-          }}
-          InputLeftElement={<Icon name="lock" size={20} ml="2" />}
-          type={show ? 'text' : 'password'}
-          InputRightElement={
-            <Icon
-              name={show ? 'eye' : 'eye-slash'}
-              size={20}
-              mr="2"
-              onPress={() => setShow(!show)}
-            />
-          }
-          placeholder="Password"
-        />
-      </Stack>
-    );
-  };
-
+  
   return (
     <Flex flex={1}>
       <Center flex={2} rounded="xl">
@@ -63,10 +23,7 @@ const Login = () => {
           <Text fontSize={40} bold color="black.100">
             Login
           </Text>
-          <InputForm />
-          <Button size="md" bold mt="4">
-            <Text>Login</Text>
-          </Button>
+          <LoginForm />
         </VStack>
       </Center>
     </Flex>
