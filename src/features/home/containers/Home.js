e9ec9abb8302d1eeeb71cd8c41ed4cc49fd5e9cc
seblopezp <seblopezp@gmail.com>
2022-08-22 22:20:23 -0400

    UPDATE: se actualiza home para mostrar info usuario

--- src/features/home/containers/Home.js
+++ src/features/home/containers/Home.js
@@ -1,7 +1,28 @@
 import React from 'react';
-import { Text, StyleSheet } from "react-native";
-const Home = () => {
-  return <Text>Home</Text>;
+import PropTypes from 'prop-types';
+import {Text} from 'react-native';
+import {Stack} from 'native-base';
+
+const Home = ({route}) => {
+  const {
+    data: {nombre, email},
+  } = route.params;
+  
+  /**
+     * TODO: se debe implementar diseño final de pantalla
+     * 
+     */
+  return (
+    <Stack space={4} w="100%" alignItems="center">
+
+    <Text>
+      hola {nombre} {email}
+    </Text>
+    </Stack>
+  );
 };
 
+Home.propTypes = {
+  route: PropTypes.any.isRequired,
+};
 export default Home;
