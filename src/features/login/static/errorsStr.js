acfb28471862373b2e6c35b4528f956541db4f74
seblopezp <seblopezp@gmail.com>
2022-09-19 11:47:23 -0300

    ADD: se agregan mensajes de error http

---
+++ src/features/login/static/errorsStr.js
@@ -0,0 +1,8 @@
+const errorStr = {
+    ERROR_LOGIN:{
+        401:{message: 'Usuario y/o contraseña incorrectos'},
+        403:{message:'Ha sucedido algo, intenta nuevamente'},
+    }
+}
+
+export default errorStr;
